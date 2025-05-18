57f0fe174dec33a269d3c9079a363f5e50df1cd5
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-18 14:32:18 +0000

    fix: replace anchor tag with Link component for client-side navigation

--- src/components/custom/sidebar/Logo.jsx
+++ src/components/custom/sidebar/Logo.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import { Link } from 'react-router'
 
 function Logo() {
     return (
-        <a href="/" className='flex items-center justify-center gap-2'>
+        <Link to="/" className='flex items-center justify-center gap-2'>
             <img src="./logo.png" alt="Quick FormX" className='size-7' />
             <span className="text-xl font-bold capitalize">Quick Formx</span>
-        </a>
+        </Link>
     )
 }
 
