ff6f30471dab7743865ec803194f5ac06cf9e113
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-19 13:24:31 +0000

    feat: add NavItems component for sidebar navigation

--- src/components/custom/sidebar/AppSidebar.jsx
+++ src/components/custom/sidebar/AppSidebar.jsx
@@ -2,31 +2,34 @@ import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, Sid
 import { ArrowUpCircleIcon } from 'lucide-react'
 import React from 'react'
 import { Logo, LogoutBtn } from '..'
+import NavItems from './NavItems'
 
 function AppSidebar() {
-  return (
-      <Sidebar collapsible="offcanvas">
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton
-              asChild
-              className="data-[slot=sidebar-menu-button]:!p-1.5"
-            >
+    return (
+        <Sidebar collapsible="offcanvas">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton
+                            asChild
+                            className="data-[slot=sidebar-menu-button]:!p-1.5"
+                        >
                 {/* app logo  */}
-             <Logo />
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarContent>
-       hello
-      </SidebarContent>
-      <SidebarFooter>
-       <LogoutBtn />
-      </SidebarFooter>
-    </Sidebar>
-  )
+                            <Logo />
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                {/* links  */}
+                <NavItems />
+            </SidebarContent>
+            <SidebarFooter>
+                {/* logout btn  */}
+                <LogoutBtn />
+            </SidebarFooter>
+        </Sidebar>
+    )
 }
 
 export default AppSidebar
