a680eee838173511eaf649a65083ad17e65bf90a
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-19 02:22:45 +0000

    feat: update UserAvatar component to use user avatar from store and add border styling

--- src/components/custom/sidebar/UserAvatar.jsx
+++ src/components/custom/sidebar/UserAvatar.jsx
@@ -1,10 +1,12 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import useStore from '@/store';
 
 function UserAvatar() {
+    const user = useStore(state=>state.user);
   return (
-   <Avatar>
-      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-      <AvatarFallback className={'capitalize'}>DA</AvatarFallback>
+   <Avatar className={'border-2 border-sky-400'}>
+      <AvatarImage src={user?.avatar?.url} alt={user?.name} />
+      <AvatarFallback className={'uppercase'}>{user?.name?.slice(0,2)}</AvatarFallback>
     </Avatar>
   )
 }
