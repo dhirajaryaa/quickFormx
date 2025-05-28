b399edd6e027211ef5d9ef665feedd151b66ac4f
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-28 13:57:44 +0000

    feat(logo): update logo image import path for consistency

--- src/components/custom/sidebar/Logo.jsx
+++ src/components/custom/sidebar/Logo.jsx
@@ -1,9 +1,10 @@
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+import Logo_Img from "@/assets/logo.png"
 
 function Logo() {
     return (
         <Link to="/" className='flex items-center justify-center gap-2'>
-            <img src="./logo.png" alt="Quick FormX" className='size-7' />
+            <img src={Logo_Img} alt="Quick FormX" className='size-7' />
             <span className="text-xl font-bold capitalize">Quick Formx</span>
         </Link>
     )
