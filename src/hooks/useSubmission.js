28a1e7793941db215c36cd50da9fb2425a9e8251
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-30 15:11:58 +0000

    refactor(submission): remove unused imports for cleaner code

--- src/hooks/useSubmission.js
+++ src/hooks/useSubmission.js
@@ -1,6 +1,5 @@
 import { saveSubmission } from "@/api/submission";
-import { getUserProfile } from "@/api/userApi";
-import { useMutation, useQuery } from "@tanstack/react-query"
+import { useMutation} from "@tanstack/react-query"
 
 export const useSubmission = () => {
     // set users
